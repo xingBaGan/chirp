8af5ed22d142c047461d1685a96af5c7a43eebd4
jacob <1063010784@qq.com>
2024-04-24 09:22:10 +0800

    clerk module

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "~/styles/globals.css";
 import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
-
+import { ClerkProvider } from '@clerk/nextjs'
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -21,10 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export default clerkMiddleware(()=>{
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
